b932997b5037ab5a2e46ce926b6e0a1e00e23305
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-07-06 00:00:27 -0400

    Create base components

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h2>About Me</h2>
+                <p>Some fun text.</p>
+            </div>
+        );
+    }
+}
+
+export default Home;
