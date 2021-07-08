1f2b21defc36b58a483784dbe1cb1a846d1799d3
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-07-08 17:19:54 -0400

    Add project details in projectArray

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,37 @@
 import { Component } from "react";
 import Project from "./Project";
 
-const projects = [
+const projectArray = [
     {
-       name: "Test",
-       gitLink: "test",
-       liveLink: "test",
-       screenshotPath: "test"
-    }
+       name: "Tech Blog",
+       gitLink: "https://github.com/Captain63/tech-blog",
+       liveLink: "https://stephentechblog.com/",
+       screenshotPath: "/assets/images/tech-blog-750px.png"
+    },
+    {
+        name: "OpenWeather Dashboard",
+        gitLink: "https://github.com/Captain63/weather-dashboard",
+        liveLink: "https://weather.stephentechblog.com/",
+        screenshotPath: "/assets/images/openweather-dashboard-750px.png"
+    },
+    {
+        name: "Work Day Scheduler Application",
+        gitLink: "https://github.com/Captain63/work-day-scheduler",
+        liveLink: "https://captain63.github.io/work-day-scheduler/",
+        screenshotPath: "/assets/images/work-day-scheduler-750px.png"
+    },
+    {
+        name: "Code Quiz Application",
+        gitLink: "https://github.com/Captain63/code-quiz",
+        liveLink: "https://captain63.github.io/code-quiz/",
+        screenshotPath: "/assets/images/code-quiz-750px.png"
+    },
+    {
+        name: "Satellite Locator Application",
+        gitLink: "https://github.com/Captain63/satellite-tracker",
+        liveLink: null,
+        screenshotPath: "/assets/images/satellite-locator-750px.png"
+    },
 ]
 
 class Projects extends Component {
@@ -15,7 +39,11 @@ class Projects extends Component {
         return (
             <div>
                 <h2>Projects</h2>
-                <Project project={projects} />
+                <div className="container">
+                    <div className="row">
+                        <Project project={projectArray} />
+                    </div>
+                </div>
             </div>
         );
     }
