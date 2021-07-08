e3b51ff2ed8a972807b51cd668e7d9341bfb2d05
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-07-08 17:21:12 -0400

    Add contact info for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,29 @@ import { Component } from "react";
 class Footer extends Component {
     render() {
         return (
-            <div>
+            <footer>
                 <h3>Get In Touch</h3>
-            </div>
+                    <section className="contact-info">
+                        <div id="phone">
+                            (571) 438-8281
+                        </div>
+                        
+                        <div id="email">
+                            <i className="far fa-envelope"></i>
+                            <a href="mailto:stephen.a.rodd@gmail.com">stephen.a.rodd@gmail.com</a>
+                        </div>
+
+                        <div id="github">
+                            <i className="fab fa-github"></i>
+                            <a href="https://github.com/Captain63" target="_blank" rel="noreferrer">GitHub profile</a>
+                        </div>
+
+                        <div id="linkedin">
+                            <i className="fab fa-linkedin"></i>
+                            <a href="https://www.linkedin.com/in/stephen-roddewig/" target="_blank" rel="noreferrer">LinkedIn profile</a>
+                        </div>
+                    </section>
+            </footer>
         );
     }
 }
